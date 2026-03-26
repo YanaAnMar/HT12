66d2ced51f8368a4ed1d9226f581bbdb21e40482
Tsveta <tsvetadzhevelekova@gmail.com>
2026-03-26 15:49:08 +0200

    django setup - eliminated two bugs

--- sunipal_project/sunipal_project/settings.py
+++ sunipal_project/sunipal_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mbf+nrb0!*dw!)(ay9x!!w4s0#hbx=j+j80r-8(pa%d1tuxv20
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.0.2.2']
 
 
 # Application definition
--- sunipal_project/sunipal_project/urls.py
+++ sunipal_project/sunipal_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('sunipal_app.urls')),
     path('sunipal_app/', include('sunipal_app.urls')),
 ]
