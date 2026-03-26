297a23627ca99b2c89c9e5bdeba4fff481f1c65f
YanaAnMar <yanamarinova10@gmail.com>
2026-03-26 14:45:20 +0200

    Created dots page

---
+++ suni_pal_app/lib/pages/dots_game.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DotsGame extends StatefulWidget {
+  const DotsGame({super.key});
+
+  @override
+  State<DotsGame> createState() => _DotsGameState();
+}
+
+class _DotsGameState extends State<DotsGame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dots'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            
+          ),
+        ),
+      )
+        
+    );
+  }
+}
